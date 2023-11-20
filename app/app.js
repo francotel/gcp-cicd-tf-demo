12e14915b1ce225eb23faabcb6ae4ed288d7599c
Franco Navarro <francotel@Francos-MBP.lan>
2023-11-20 16:17:49 -0600

    first commit ci nodejs v12

--- app/app.js
+++ app/app.js
@@ -5,16 +5,21 @@ const moment = require('moment-timezone');
 
 const app = express();
 
+// Ruta para obtener la hora de Lima y Austin
 app.get('/', (req, res) => {
+  // Obtener la hora actual de Lima y Austin
   const limaTime = moment.tz('America/Lima').format('YYYY-MM-DD HH:mm:ss');
   const austinTime = moment.tz('America/Chicago').format('YYYY-MM-DD HH:mm:ss');
-  
-  const responseText = `Hora de Lima: ${limaTime}\n Hora de Austin: ${austinTime}`;
-  res.send(responseText);
+
+  // Enviar la hora al cliente como JSON
+  res.json({ limaTime, austinTime });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+// Iniciar el servidor en el puerto 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
 
 module.exports = app; // Exportar la aplicación para pruebas
