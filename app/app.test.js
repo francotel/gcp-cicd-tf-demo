5c1640fccd04ea7aab679b60f316c345e7acf10c
Franco Navarro <francotel@Francos-MBP.lan>
2023-11-20 16:06:19 -0600

    first commit ci nodejs v8

--- app/app.test.js
+++ app/app.test.js
@@ -7,7 +7,7 @@ describe('GET /', () => {
   it('responds with status 200 and contains time information', async () => {
     const response = await request(app).get('/');
     
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(300);
     expect(response.text).toContain('Hora de Lima');
     expect(response.text).toContain('Hora de Austin');
   });
