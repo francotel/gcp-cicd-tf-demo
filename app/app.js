9bc6a992e960db3df097f3094920d2f2a14cd416
Franco Navarro <francotel@Francos-MBP.lan>
2023-11-30 14:40:48 -0600

    add new header v1

--- app/app.js
+++ app/app.js
@@ -13,8 +13,13 @@ app.get('/', (req, res) => {
   const austinTime = moment.tz('America/Chicago').format('YYYY-MM-DD HH:mm:ss');
 
   // Enviar la hora al cliente como JSON
+  res.setHeader('Content-Type', 'application/json');
   res.json({ limaTime, austinTime });
 });
+
+app.get("/ping", (req, res) => {
+  res.send({ success: true })
+})
 
 // Iniciar el servidor en el puerto 3000
 const PORT = 3000;
@@ -22,5 +27,7 @@ app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+
+
 
 module.exports = app; // Exportar la aplicación para pruebas
