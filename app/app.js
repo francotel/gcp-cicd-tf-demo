90fd3cc261de4b088682716eadcf41ff1aefd44e
Franco Navarro <francotel@Francos-MBP.lan>
2023-11-30 14:52:00 -0600

    add new header v2

--- app/app.js
+++ app/app.js
@@ -1,20 +1,17 @@
-// app.js
-'use strict';
-
-const express = require('express');
+const express = require("express")
 const moment = require('moment-timezone');
 
-const app = express();
+const app = express()
 
 // Ruta para obtener la hora de Lima y Austin
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // Obtener la hora actual de Lima y Austin
   const limaTime = moment.tz('America/Lima').format('YYYY-MM-DD HH:mm:ss');
   const austinTime = moment.tz('America/Chicago').format('YYYY-MM-DD HH:mm:ss');
 
   // Enviar la hora al cliente como JSON
   res.setHeader('Content-Type', 'application/json');
-  res.json({ limaTime, austinTime });
+  res.send(JSON.stringify({ limaTime, austinTime }));
 });
 
 app.get("/ping", (req, res) => {
