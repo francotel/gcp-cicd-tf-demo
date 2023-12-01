bcb6f1fd006af76ebc3dfdb9a6826a979967be8b
Franco Navarro <francotel@Francos-MBP.lan>
2023-12-01 08:18:15 -0600

    add new cicd v31

--- app/app.js
+++ app/app.js
@@ -8,11 +8,11 @@ app.get("/", (req, res) => {
   // Obtener la hora actual de Lima y Austin
   const limaTime = moment.tz('America/Lima').format('YYYY-MM-DD HH:mm:ss');
   const austinTime = moment.tz('America/Chicago').format('YYYY-MM-DD HH:mm:ss');
-  const santiagoTime = moment.tz('America/Santiago').format('YYYY-MM-DD HH:mm:ss')
+  //const santiagoTime = moment.tz('America/Santiago').format('YYYY-MM-DD HH:mm:ss')
 
   // Enviar la hora al cliente como JSON
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ limaTime, austinTime , santiagoTime}));
+  res.send(JSON.stringify({ limaTime, austinTime}));
 });
 
 app.get("/ping", (req, res) => {
